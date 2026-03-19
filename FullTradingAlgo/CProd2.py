be927a476f368a5a9efb4db94407f0c5a1b423f1
jeromedumont35 <jerome.dumont35@gmail.com>
2026-03-19 07:38:53 +0100

    Refactor kline fetching and strategy execution

--- FullTradingAlgo/CProd2.py
+++ FullTradingAlgo/CProd2.py
@@ -64,46 +64,9 @@ class CProd2:
                 print(f"\n⏰ Nouvelle minute détectée : {now}")
                 time.sleep(3)  # Attendre la publication de la bougie
 
-                l_bougies = self.fetcher.get_last_complete_kline(self.symbols, interval=self.interval)
-
-                if df_last.empty:
-                    print("⚠️ Pas de nouvelle bougie dispo (retard API ?).")
-                    time.sleep(1)
-                    continue
-
-                list_data_last = []
-
-                for sym in self.symbols:
-                    df_sym = self.symbol_dfs[sym]
-                    df_new = df_last[df_last["symbol"] == sym].drop(columns=["symbol"])
-
-                    df_new = self.extend_df_with_sym(df_sym, df_new)
-
-                    # Gestion des gaps temporels
-                    if not df_sym.empty:
-                        last_time = df_sym.index[-1]
-                        new_time = df_new.index[-1]
-                        expected_time = last_time + timedelta(minutes=1)
-                        if expected_time < new_time:
-                            print(f"⚠️ Gap détecté pour {sym}: {expected_time} -> {new_time}")
-                            n_missing = int((new_time - expected_time).total_seconds() / 60)
-                            for i in range(n_missing):
-                                missing_time = expected_time + timedelta(minutes=i)
-                                missing_row = df_sym.iloc[[-1]].copy()
-                                missing_row.index = [missing_time]
-                                df_sym = pd.concat([df_sym, missing_row])
-
-                    # Ajout nouvelle bougie
-                    df_sym = pd.concat([df_sym.iloc[1:], df_new])
-
-                    # Réappliquer indicateurs
-                    df_sym = self.algo.strategy.apply_indicators(df_sym, is_btc_file=(sym == "BTCUSDC"))
-                    self.symbol_dfs[sym] = df_sym
-
-                    df_last_with_ind = df_sym.tail(1)
-                    list_data_last.append((df_last_with_ind, sym))
+                l_bougies = self.fetcher._fetch_klines3(self.symbols, interval=self.interval,1000)
 
                 # Exécution stratégie sur dernière bougie
-                self.algo.run(bougies, execution=True)
+                self.algo.run(l_bougies, execution=True)
 
             time.sleep(0.5)
