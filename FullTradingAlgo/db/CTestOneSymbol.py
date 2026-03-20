16c2030cff5472077d69607139565a85a0217894
jeromedumont35 <jerome.dumont35@gmail.com>
2026-03-20 22:40:13 +0100

    Update CTestOneSymbol.py

--- FullTradingAlgo/db/CTestOneSymbol.py
+++ FullTradingAlgo/db/CTestOneSymbol.py
@@ -2,6 +2,7 @@ import os
 import time
 import requests
 from pathlib import Path
+from CLauncher3 import CLauncher3
 
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
@@ -15,6 +16,7 @@ class CTestOneSymbol:
     def __init__(self):
         self.test_above_trend = CTestAboveTrend()
         self.test_rsi5min_madays = CTestRSI5Min_MADays()
+        self.launcher = CLauncher3()
 
 
     def realiser(self, DB, dfoneminute, symbol):
@@ -22,5 +24,8 @@ class CTestOneSymbol:
         #print(f"Analyse du symbole : {symbol}")
 
         result = self.test_rsi5min_madays.realiser(DB, dfoneminute, symbol)
+
+        if result > 70:
+            self.launcher.run_launcher(amount=6,symbol=symbol)
 
         return result
